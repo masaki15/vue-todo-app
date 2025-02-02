456e2dce4435851977f716df1960b9ea4fd4b878
masaki15 <msk1305@protonmail.com>
2025-02-02 12:01:20 +0900

    feat(ui): add Sing up button

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -9,7 +9,8 @@
         <v-text-field class="mb-2" label="Email" clearable></v-text-field>
         <v-text-field label="Password"></v-text-field>
       </v-form>
-      <v-btn class="bg-green-darken-1" block>Sign In</v-btn>
+      <v-btn class="bg-green-darken-1 mb-2" block>Sign In</v-btn>
+      <v-btn class="bg-blue" block>Sign Up</v-btn>
     </v-card>
   </v-sheet>
 </template>
